d7c3f2df0f8bbc8a0735f4d3485e68de2653f236
Ahmed Refaat <ahmedr162018010@gmail.com>
2026-04-27 12:14:58 +0300

    Initial commit - Entities and Enum

---
+++ ECommerceAPI.Domain/Entities/Cart.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceAPI.Domain.Entities
+{
+    internal class Cart
+    {
+    }
+}
---
+++ ECommerceAPI.Domain/Enums/Enums.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceAPI.Domain.Enums
+{
+    internal class Enums
+    {
+    }
+}
